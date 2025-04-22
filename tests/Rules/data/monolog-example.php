166802c81f53babdf59aa460382fbe4e1314a360
Markus Staab <maggus.staab@googlemail.com>
2025-04-22 18:17:31 +0200

    Don't error on safe types (#12)

--- tests/Rules/data/monolog-example.php
+++ tests/Rules/data/monolog-example.php
@@ -6,12 +6,12 @@ use Monolog\Level;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-function basicExample() {
+function basicExample(string $s) {
     // create a log channel
     $log = new Logger('name');
     $log->pushHandler(new StreamHandler('path/to/your.log', Level::Warning));
 
-    $s = 'foo';
+
 
     $log->warning('Foo'.$s);
     $log->error('Bar'.$s);
